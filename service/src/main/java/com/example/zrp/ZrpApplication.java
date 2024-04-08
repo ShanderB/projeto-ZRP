20eb3e6b4318b844482ff8d6b669365f715af785
ShanderB <s.hander8910@hotmail.com>
2024-04-08 19:44:00 -0300

    Init service

---
+++ service/src/main/java/com/example/zrp/ZrpApplication.java
@@ -0,0 +1,13 @@
+package com.example.zrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZrpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZrpApplication.class, args);
+	}
+
+}
