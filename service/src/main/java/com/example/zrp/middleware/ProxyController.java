3392bc29201f4a6948a47cd0c46c27d481663918
ShanderB <s.hander8910@hotmail.com>
2024-04-08 20:34:30 -0300

    Creation of middleware controller with the API.

---
+++ service/src/main/java/com/example/zrp/middleware/ProxyController.java
@@ -0,0 +1,31 @@
+package com.example.zrp.middleware;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+public class ProxyController {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${pokemon.api.url}")
+    private String apiUrlString;
+
+    public ProxyController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/api")
+    public ResponseEntity<String> proxy(@RequestParam(required = false) MultiValueMap<String, String> param) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(apiUrlString).queryParams(param);
+        
+        ResponseEntity<String> response = restTemplate.getForEntity(builder.toUriString(), String.class);
+        return response;
+    }
+}
