19e8c46ccfd91ee4e3b04040e6d68c57638eac81
ShanderB <s.hander8910@hotmail.com>
2024-04-09 08:43:52 -0300

    Uppercase first letter.

--- webapp/src/components/results/Results.tsx
+++ webapp/src/components/results/Results.tsx
@@ -5,9 +5,11 @@ function Results({ result, handleToggleFavorite, favorites }) {
     <div className="results-container">
       {result && result.map((item, index) => {
         const isFavorite = favorites.some((favorite) => favorite.name === item.name);
+        const name = item.name.charAt(0).toUpperCase() + item.name.slice(1); // Make the first letter uppercase
+
         return (
           <div key={index} className="result-item">
-            <span className="result-name">{item.name}</span>
+            <span className="result-name">{name}</span>
             <button 
               className={`toggle-favorite-button ${isFavorite ? 'favorite' : ''}`} 
               onClick={() => handleToggleFavorite(item)}
