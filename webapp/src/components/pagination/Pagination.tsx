ed6196dddec05def1ff26550c3d3796bab182778
ShanderB <s.hander8910@hotmail.com>
2024-04-09 10:55:36 -0300

    Moved fetch all outside flex.

--- webapp/src/components/pagination/Pagination.tsx
+++ webapp/src/components/pagination/Pagination.tsx
@@ -5,11 +5,14 @@ import { PaginationProps } from '../../interfaces/Pagination';
 
 const Pagination: React.FC<PaginationProps> = ({ nextUrl, prevUrl, fetchData, API }) => {
   return (
-    <div className="button-container">
-      <button className="fetch-button" onClick={() => fetchData(API)} hidden={!!nextUrl || !!prevUrl}>Fetch</button>
-      <button className="next-button" onClick={() => nextUrl && fetchData(nextUrl)} hidden={!nextUrl}>Next</button>
-      <button className="prev-button" onClick={() => prevUrl && fetchData(prevUrl)} hidden={!prevUrl}>Previous</button>
-    </div>
+    <>
+      <button className="fetch-button" onClick={() => fetchData(API)} hidden={!!nextUrl || !!prevUrl}>Fetch All</button>
+
+      <div className="button-container">
+        <button className="next-button" onClick={() => nextUrl && fetchData(nextUrl)} hidden={!nextUrl}>Next</button>
+        <button className="prev-button" onClick={() => prevUrl && fetchData(prevUrl)} hidden={!prevUrl}>Previous</button>
+      </div>
+    </>
   );
 };
 
