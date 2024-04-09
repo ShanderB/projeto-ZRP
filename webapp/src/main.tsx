d0b30f6b1ae7b33efe7b6218a663ca2db3eb8b3a
ShanderB <s.hander8910@hotmail.com>
2024-04-09 08:05:42 -0300

    Refresh the console every refresh.

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -5,6 +5,14 @@ import './index.css'
 import { Provider } from 'react-redux';
 import { store } from './state/store/store';
 
+if (import.meta.hot) {
+  import.meta.hot.on(
+    "vite:beforeUpdate",
+    /* eslint-disable-next-line no-console */
+    () => console.clear()
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <App />
